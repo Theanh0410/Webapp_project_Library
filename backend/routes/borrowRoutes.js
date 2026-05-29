d646b2a4770ed8a5692e37c077d0f191e69e8894
SkibidiSigmaEdgeGyatGoonOhio <ITCSIU22020@student.hcmiu.edu.vn>
2026-05-29 22:05:51 +0700

    Add files via upload

--- backend/routes/borrowRoutes.js
+++ backend/routes/borrowRoutes.js
@@ -4,6 +4,10 @@ import {
   createBorrowRecord,
   returnBorrowRecord,
   getBorrowReminders,
+  getPendingApprovals,
+  approveBorrowRequest,
+  getPendingReturns,
+  approveReturnRequest,
 } from "../controllers/borrowController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -12,6 +16,10 @@ const router = express.Router();
 router.get("/", protect, getBorrowRecords);
 router.post("/", protect, createBorrowRecord);
 router.get("/reminders", protect, getBorrowReminders);
+router.get("/pending-approvals", protect, getPendingApprovals);
+router.get("/pending-returns", protect, getPendingReturns);
 router.put("/:id/return", protect, returnBorrowRecord);
+router.put("/:id/approve", protect, approveBorrowRequest);
+router.put("/:id/approve-return", protect, approveReturnRequest);
 
 export default router;
